e3a574457b54525e73373c30fe1ae2f0ef433773
MarFerDom <m.domingues@poli.ufrj.br>
2026-06-24 18:18:16 +0200

    Correction for stopping server from outside the Thread

--- src/server/server.py
+++ src/server/server.py
@@ -12,6 +12,7 @@ import os
 #                                                                           #
 #############################################################################
 
+# Get Host and Port from the environment variables.
 HOST:str = os.getenv('HOST') or "127.0.0.1"
 PORT:int = int(os.getenv('PORT') or 65000)
 
@@ -32,21 +33,29 @@ class Server(threading.Thread):
         self.host = host
         self.port = port
         self.server = None
-        self.kill_switch = threading.Event()
+        # Event is used to hold the parallel task that closes the server
+        # from continuing. It continues after kill_switch.set()
+        self.kill_switch = asyncio.Event()
 
     def run(self):
+        # Run is called by Thread.start().
+        # This is asyncio starting point
         asyncio.run(self.start_server())
 
     async def start_server(self) -> None:
+        # Start server that produces Tasks to serve requests
         self.server = await asyncio.start_server(
             self.accept_connections, self.host, self.port
         )
+        # Get current Loop and add parallel Task.
+        # stop_serving closes the server but first awaits for the kill_switch.
         self.loop = asyncio.get_running_loop()
         self.loop.create_task(self.stop_serving())
 
         host, port, *_ = self.server.sockets[0].getsockname()
         logging.info(f"Server started at http://{host}:{port}")
 
+        # Start the Loop that only stops if server is cancelled.
         async with self.server:
             try:
                 await self.server.serve_forever()
@@ -57,17 +66,26 @@ class Server(threading.Thread):
                                  reader: asyncio.StreamReader,
                                  writer: asyncio.StreamWriter
                                  ) -> None:
+        '''
+            Task that serves requests.
+
+            TODO: Substitute for plugin design pattern to extend functionality
+        '''
+        # Read requests
         request = await reader.read(REQ_SIZE)
+        logging.info(f"Received {request.decode()}")
         await asyncio.sleep(2)
+        # Write response
         writer.write(request)
         await writer.drain()
         writer.close()
 
-    def stop_server(self):
-        self.kill_switch.set()
-
     async def stop_serving(self) -> None:
-        self.kill_switch.wait()
+        '''
+            Run as a parallel Task to close server after waiting
+            for the Event kill_switch to be set.
+        '''
+        await self.kill_switch.wait()
         logging.info('Kill switch was set. Stopping all async tasks')
         if self.server is not None:
             self.server.close()
@@ -76,9 +94,16 @@ class Server(threading.Thread):
             for task in asyncio.all_tasks(loop=self.loop):
                 task.cancel()
 
-
+    def stop_server(self):
+        '''
+            Insert parallel Task to set the kill_switch into Loop
+            from outside of the thread.
+        '''
+        self.loop.call_soon_threadsafe(self.kill_switch.set)
 
 if __name__ == "__main__":
+    # Start server and translate a KeyboardInterrupt into
+    # stopping the server
     server = Server()
     server.start()
     try:
--- test/integration_test/test_server.py
+++ test/integration_test/test_server.py
@@ -1,13 +1,16 @@
 import pytest
 import socket
 import json
+import logging
 from os import path, getenv, getcwd
-from sys import argv
+from sys import argv, _getframe
 
 HOST = getenv("HOST") or "127.0.0.1"
 PORT = getenv("PORT") or 65000
 # path.dirname(argv[0]) does not work because of env
-REQ_RESP_FILEPATH = path.join(getcwd(), 'test', 'test_send_recv.json')
+REQ_RESP_FILEPATH = path.join(getcwd(), 'test', 'integration_test', 'test_send_recv.json')
+
+logging.basicConfig(level=logging.INFO)
 
 def send_recv_test_wrapper(payload, right_response):
     # Wrapper for request and response
@@ -19,9 +22,11 @@ def send_recv_test_wrapper(payload, right_response):
 
             # Send request
             client_sock.sendall(payload.encode())
+            print(f"{_getframe().f_code.co_name}: Sending payload")
 
             # Get response
             response = client_sock.recv(1024)
+            print(f"{_getframe().f_code.co_name}: Received reponse")
 
             # Assert correct reponse
             assert response.decode() == right_response, f"Response mismatch\n{response : }\n{right_response : }"
@@ -31,3 +36,11 @@ with open(REQ_RESP_FILEPATH, 'r') as f:
     test_payloads = json.load(f)
     for test in test_payloads["test_io"]:
         globals()[test['name']] = send_recv_test_wrapper(payload=test['payload'], right_response=test['response'])
+
+if __name__ == "__main__":
+    items = dict(filter(
+        lambda item: item[0].startswith("test_") and callable(item[1]),
+        globals().items()
+    ))
+    for call in items.values():
+        call()
